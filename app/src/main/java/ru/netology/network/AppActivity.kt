614fac3b71bc3d0adf833c39e6a47cd77b99f4d2
StanAtilovich <alano92@yandex.ru>
2023-06-27 18:08:15 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/network/AppActivity.kt
@@ -0,0 +1,13 @@
+package ru.netology.network
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ru/netology/network/ui/post/PostFeedFragment.kt
@@ -0,0 +1,4 @@
+package ru.netology.network.ui.post
+
+class PostFeedFragment {
+}
